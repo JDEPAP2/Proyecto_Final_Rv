b347e1b713dd8a94c55ff535e6d40a09b71dd471
JDEPAP2 <jdepap2@gmail.com>
2023-11-17 16:52:45 -0500

    Prueba Oculus

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class Counter : MonoBehaviour
+{
+    public float seconds;
+    private float time;
+    public UnityEvent onTimeFinished, onTimerPaused, onTimerUnpaused, onTimerRestarted;
+    TextMeshProUGUI txt;
+    Coroutine coroutine;
+
+    void Start()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+        coroutine = StartCoroutine(Timer(seconds));
+    }
+
+    public void PauseTimer(bool state)
+    {
+        if (state)
+        {
+            StopCoroutine(coroutine);
+            onTimerPaused.Invoke();
+        }
+        else
+        {
+            coroutine = StartCoroutine(Timer(time));
+            onTimerUnpaused.Invoke();
+        }
+    }
+
+    public void ResetTimer()
+    {
+        StopCoroutine(coroutine);
+        coroutine = StartCoroutine(Timer(seconds));
+        onTimerRestarted.Invoke();
+    }
+
+
+    IEnumerator Timer(float seconds)
+    {
+        time = seconds;
+        while(time > 0)
+        {
+            txt.SetText(time.ToString());
+            time--;
+            yield return new WaitForSeconds(1);
+        }
+        onTimeFinished.Invoke();
+    }
+}
